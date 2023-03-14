2b9655e2a8ebc3c164894547be7d15d309554101
MadhuMohan231 <127888637+MadhuMohan231@users.noreply.github.com>
2023-03-15 00:47:03 +0530

    Update MirroredRightTrianglePattern.java
    This problems provides us with the logic of creating Mirrored Right angled Triangle pattern using java program

--- Star_Pattern_Problems/MirroredRightTrianglePattern.java
+++ Star_Pattern_Problems/MirroredRightTrianglePattern.java
@@ -1,8 +1,7 @@
 public class MirroredRightTrianglePattern   
 {  
 public static void main(String[] args)  
-{  
-int n=7;  
+{  int n=7;  
 for (int i= 0; i<= n; i++)  
 {    
 for (int j=1; j<=n-i; j++)  
